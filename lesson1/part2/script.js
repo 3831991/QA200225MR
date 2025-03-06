c9653387371de446b90c4cca77db8129e19ffda1
Your Name <you@example.com>
2025-03-06 15:50:59 +0200

    added

--- lesson1/part2/script.js
+++ lesson1/part2/script.js
@@ -13,3 +13,23 @@ function multi50() {
 
     alert(x * 50);
 }
+
+function multi() {
+    let x = document.getElementById("num1").value;
+    let y = document.getElementById("num2").value;
+
+    alert(x * y);
+}
+
+function addition() {
+    let n1 = +document.getElementById("num3").value;
+    let n2 = +document.getElementById("num4").value;
+
+    alert(n1 + n2);
+}
+
+function welcomeUser2() {
+    let userName = document.getElementById("name2").value;
+
+    document.getElementById("output").innerText = "שלום ל" + userName;
+}
