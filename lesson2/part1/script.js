62997024f4096fc728ce57b4dbe1451c054a613f
Your Name <you@example.com>
2025-03-09 09:55:29 +0200

    added

--- lesson2/part1/script.js
+++ lesson2/part1/script.js
@@ -2,3 +2,7 @@ function message() {
     alert("ברוכים הבאים לאתר שלנו!");
 }
 
+function message2() {
+    let name = document.getElementById("name").value;
+    alert("שלום " + name + " ברוך הבא לאתר שלנו!")
+}
