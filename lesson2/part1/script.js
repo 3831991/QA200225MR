16fad451913cab7c938b5c64dc279666d7f9ed5a
Your Name <you@example.com>
2025-03-09 10:41:02 +0200

    added

--- lesson2/part1/script.js
+++ lesson2/part1/script.js
@@ -6,3 +6,25 @@ function message2() {
     let name = document.getElementById("name").value;
     alert("שלום " + name + " ברוך הבא לאתר שלנו!")
 }
+
+function multi50() {
+    let n = document.getElementById("num").value;
+    alert(n * 50);
+}
+
+function multi() {
+    let n1 = document.getElementById("num1").value;
+    let n2 = document.getElementById("num2").value;
+    alert(n1 * n2);
+}
+
+function sum() {
+    let n1 = document.getElementById("num3").value;
+    let n2 = document.getElementById("num4").value;
+    alert(+n1 + +n2);
+}
+
+function message3() {
+    let name = document.getElementById("username").value;
+    document.getElementById("title").innerText = "שלום " + name + " ברוך הבא לאתר שלנו!";
+}
