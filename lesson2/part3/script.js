89a5834941dd8ad13580a1388f7f6f2966e908e8
Your Name <you@example.com>
2025-03-09 14:25:21 +0200

    added

---
+++ lesson2/part3/script.js
@@ -0,0 +1,35 @@
+function plus() {
+    let n1 = +document.getElementById("num1").value;
+    let n2 = +document.getElementById("num2").value;
+
+    document.getElementById("output1").innerHTML = n1 + n2;
+}
+
+function minus() {
+    let n1 = +document.getElementById("num1").value;
+    let n2 = +document.getElementById("num2").value;
+
+    document.getElementById("output1").innerHTML = n1 - n2;
+}
+
+function multi() {
+    let n1 = +document.getElementById("num1").value;
+    let n2 = +document.getElementById("num2").value;
+
+    document.getElementById("output1").innerHTML = n1 * n2;
+}
+
+function division() {
+    let n1 = +document.getElementById("num1").value;
+    let n2 = +document.getElementById("num2").value;
+
+    document.getElementById("output1").innerHTML = n1 / n2;
+}
+
+function task2() {
+    let n1 = +document.getElementById("num3").value;
+    let n2 = +document.getElementById("num4").value;
+    let op = document.getElementById("op").value;
+
+    
+}
