d0b8f5f61ccbfdda9d4a9c8ef64bbf95b315bf5d
Your Name <you@example.com>
2025-03-09 11:19:05 +0200

    added

---
+++ lesson2/part2/script.js
@@ -0,0 +1,9 @@
+function task1() {
+    let city = document.getElementById("city").value;
+
+    if (city == "קרית ארבע") {
+        alert("נכון מאוד");
+    } else {
+        alert("לא נכון");
+    }
+}
