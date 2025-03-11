2c3d0370ad2b2174d781be75e120683d02c0e914
Your Name <you@example.com>
2025-03-11 13:05:01 +0200

    added

---
+++ lesson3/part2/script.js
@@ -0,0 +1,48 @@
+function task1() {
+    const rand = Math.random();
+    const num = Math.floor(rand * 15) + 1;
+
+    document.getElementById("output1").innerHTML = num;
+}
+
+function task2() {
+    const rand = Math.random();
+    const num = Math.floor(rand * 11) + 10;
+
+    document.getElementById("output2").innerHTML += num + ', ';
+}
+
+function dice() {
+    const rand = Math.random();
+    const num = Math.floor(rand * 6) + 1;
+    const d = document.getElementById("dice");
+
+    if (num == 1) {
+        d.innerHTML = '<span class="pip"></span>';
+    } else if (num == 2) {
+        d.innerHTML = `<span class="pip"></span>
+                       <span class="pip"></span>`;
+    } else if (num == 3) {
+        d.innerHTML = `<span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>`;
+    } else if (num == 4) {
+        d.innerHTML = `<span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>`;
+    } else if (num == 5) {
+        d.innerHTML = `<span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>`;
+    } else if (num == 6) {
+        d.innerHTML = `<span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>
+                       <span class="pip"></span>`;
+    }
+}
