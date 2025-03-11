ff481dc02d3858f72ec0b55a7086eb5cee728c31
Your Name <you@example.com>
2025-03-11 13:50:13 +0200

    sdded

--- lesson3/part3/script.js
+++ lesson3/part3/script.js
@@ -1,5 +1,18 @@
 const students = ["יעקב", "אנה", "אווה", "אמיר", "לאריק", "ג'ניה", "אלישיב"];
 
 function task1() {
+    document.getElementById("output1").innerHTML = students.join(', ');
+}
+
+function task2() {
+    document.getElementById("output2").innerHTML = students.join('<br>');
+}
+
+function task3() {
+    document.getElementById("output3").innerHTML = [...students].sort().join('<br>');
+}
+
+function task4() {
+    const i = +document.getElementById("index").value;
     
 }
