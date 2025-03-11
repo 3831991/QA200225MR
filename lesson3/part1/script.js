2cc653a41b357b181955fdf746af81341e7c8747
Your Name <you@example.com>
2025-03-11 10:15:33 +0200

    added

--- lesson3/part1/script.js
+++ lesson3/part1/script.js
@@ -7,12 +7,20 @@ function salerycalcultor() {
         newSalery = salery * 1.05;
     }
 
-    document.getElementById("output1").innerHTML = `המשכורת החדשה של ${user} היא ${newSalery} ש"ח.`;
+    document.getElementById("output1").innerHTML = `המשכורת החדשה של ${user} היא ${newSalery} ₪.`;
 }
 
 function check() {
     let age = +document.getElementById('age').value;
     let height = +document.getElementById('height').value;
 
-    
+    let isHeight = height >= 182;
+    let isRange1 = age >= 14 && age <= 18;
+    let isRange2 = age >= 21 && age <= 26;
+
+    if (isHeight && (isRange1 || isRange2)) {
+        document.getElementById("output2").innerHTML = "התקבל";
+    } else {
+        document.getElementById("output2").innerHTML = "לא התקבל";
+    }
 }
