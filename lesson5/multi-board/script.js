deb30d72e9c3bb6b038e876333c91c3f68227bb6
Your Name <you@example.com>
2025-03-20 12:48:42 +0200

    added

--- lesson5/multi-board/script.js
+++ lesson5/multi-board/script.js
@@ -1,3 +1,11 @@
 function createMultiBoard() {
-    
+    const arr = [];
+
+    for (let x = 1; x <= 10; x++) {
+        for (let y = 1; y <= 10; y++) {
+            arr.push(`<div>${x * y}</div>`);
+        }
+    }
+
+    document.getElementById("board").innerHTML = arr.join("");
 }
